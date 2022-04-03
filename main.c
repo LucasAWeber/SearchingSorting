b7c632414e4eaca9018b3e238acd3e33dad1014b
Lucas Weber <lweber07@uoguelph.ca>
2022-04-03 15:32:49 -0400

    added main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+int main(void)
+{
+    int input;
+
+    do
+    {
+        printf("1. Searching\n2. Sorting\nChoice: ");
+        scanf("%d", &input);
+        switch(input)
+        {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+        }
+    } while (input != 3);
+    
+
+    return 0;
+}
